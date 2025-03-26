9b1307d685fc3dfbf5e17b35e14b0e7b26e15671
badin <badinovahanna@gmail.com>
2025-03-26 11:05:23 +0100

    Add Contact.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,44 +1,42 @@
-import React, {useEffect} from 'react';
-import {base_url} from "../utils/constants.js";
-import {useState } from 'react';
-import {characterDetails} from "../utils/constants.js";
 
+import {base_url} from "../utils/constants.js";
+import {useEffect, useState} from "react";
 
 const AboutMe = () => {
-    const [hero, setHero] = useState('Loaning...');
-
+    const [hero, setHero] = useState();
     useEffect(() => {
         fetch(`${base_url}/v1/peoples/1`)
-    .then((response) => {
-            if (!response.ok) {
-                throw new Error("Failed to fetch hero");
-            }
-            return response.json();
-        })
-            .then((data) => setHero(data))
-            .catch((error) => {
-                setHero({ name: "Error loading data" });
-                console.error(error);
-            });
-    }, []);
-
-    if (hero.name === 'Loading...') {
-        return <p>Loading...</p>;
-    }
-
-    if (hero.name === "Error loading data") {
-        return <p>Failed to load hero data. Please try again later.</p>;
-    }
+            .then(response => response.json())
+            .then(data => {
+                const info = {
+                    name: data.name,
+                    gender: data.gender,
+                    birth_year: data.birth_year,
+                    height: data.height,
+                    mass: data.mass,
+                    hair_color: data.hair_color,
+                    skin_color: data.skin_color,
+                    eye_color: data.eye_color
+                }
+                setHero(info);
+            })
+    }, [])
 
     return (
-        <div className="aboutMe">
-            <h2>{hero.name}</h2>
-            {characterDetails.map(({ label, key, unit }) => (
-                <p key={key}>
-                    <strong>{label}:</strong> {hero[key]}{unit}
-                </p>
-            ))}
-        </div>
+        <>
+            {(!!hero) &&
+                <div className='fs-2 lh-lg text-justify ms-5'>
+                    <p><span className='display-3'>name:</span> {hero.name}</p>
+                    <p><span className='display-3'>gender:</span> {hero.gender}</p>
+                    <p><span className='display-3'>birth year:</span> {hero.birth_year}</p>
+                    <p><span className='display-3'>height:</span> {hero.height}</p>
+                    <p><span className='display-3'>mass:</span> {hero.mass}</p>
+                    <p><span className='display-3'>hair color:</span> {hero.hair_color}</p>
+                    <p><span className='display-3'>skin color:</span> {hero.skin_color}</p>
+                    <p><span className='display-3'>eye color:</span> {hero.eye_color}</p>
+                </div>
+            }
+        </>
     );
 };
 
